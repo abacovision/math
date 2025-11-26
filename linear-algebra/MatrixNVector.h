8b5985af6a921b6440ac500f4373f0422234616b
Filipy <filipydasilvacorporativo@gmail.com>
2025-11-26 16:13:22 -0300

    Added: created matrixvector.h header

---
+++ linear-algebra/MatrixNVector.h
@@ -0,0 +1,20 @@
+//
+// Created by: Filipy
+// Reference: https://web.mit.edu/15.053/www/AMP-Appendix-A.pdf
+//
+
+#ifndef MATH_MATRIXNVECTOR_H
+#define MATH_MATRIXNVECTOR_H
+
+public class MatrixNVector{
+    int componentNumbers;
+    struct Vector{
+        int rowNumber;
+        int values[rowNumber];
+    }
+
+
+}
+
+
+#endif
